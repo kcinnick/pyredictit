5b53a827a0888a32e9c6b0d14c51f04f012cf0a1
Nick Tucker <nicktucker4@gmail.com>
2018-12-16 16:34:10 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pyredictit',
+    version='0.1',
+    packages=[],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
